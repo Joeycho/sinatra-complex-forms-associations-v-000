3fab2cbb6c0445ccd4d6acfbcf4a2d53a7cb8151
Joey Cho <cjy12182000@naver.com>
2019-01-30 13:54:56 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,8 +12,8 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(:name =>params["pet_name"])
-    binding.pry
     if params["owner"] == nil
+      binding.pry
       @pet.owner = Owner.create(:name => params["owner_name"])
       @pet.save
     else
